d68d999020360b39fa351faa4547f18cabe87748
8mitsuboy <hachimitsu8897@icloud.com>
2025-11-21 13:09:18 +0900

    JokePageをConsumerWidgetに変更し、ViewModelを使用して状態管理を実装

--- lib/features/joke_page.dart
+++ lib/features/joke_page.dart
@@ -1,67 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:mvvm_practice/di.dart';
-import 'package:mvvm_practice/domain/joke.dart';
-import 'package:mvvm_practice/domain/joke_reposeitory.dart';
-import 'package:mvvm_practice/domain/joke_repository_impl.dart';
-import '../data/joke_api_datasource.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mvvm_practice/application/joke_view_model.dart';
 
-class JokePage extends StatefulWidget {
+class JokePage extends ConsumerWidget {
   const JokePage({super.key});
 
   @override
-  State<JokePage> createState() => _JokePageState();
-}
-
-class _JokePageState extends State<JokePage> {
-  final JokeReposeitory _jokeReposeitory = DI.buildRepository();
-
-  Joke? _joke;
-  bool _loading = false;
-  String? _error;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetch();
-  }
-
-  Future<void> _fetch() async {
-    setState(() {
-      _loading = true;
-      _error = null;
-    });
-
-    try {
-      final joke = await _jokeReposeitory.fetchRandomJoke();
-      setState(() => _joke = joke);
-    } catch (e) {
-      setState(() => _error = e.toString());
-    } finally {
-      setState(() => _loading = false);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_loading) return const Center(child: CircularProgressIndicator());
-    if (_error != null) return Center(child: Text('Error: $_error'));
-    if (_joke == null) return const Center(child: Text('No joke'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ViewModel の状態 (AsyncValue<Joke>) を取得
+    final state = ref.watch(jokeViewModelProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Joke App")),
+      appBar: AppBar(title: const Text("Joke app")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              '${_joke!.setup}\n\n${_joke!.punchline}',
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(onPressed: _fetch, child: const Text('Another')),
-          ],
+        child: state.when(
+          data: (joke) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '${joke.setup}\n\n${joke.punchline}',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(jokeViewModelProvider.notifier).reload();
+                },
+                child: const Text('Another'),
+              ),
+            ],
+          ),
+          error: (e, _) => Text('Error: $e'),
+          loading: () => const CircularProgressIndicator(),
         ),
       ),
     );
--- question.md
+++ question.md
@@ -17,3 +17,20 @@ Dart の test パッケージに、trhwosA()という例外マッチャーがあ
 `throwsA(isA<JokeApiException>())` 第一引数の関数は JokeApiException 型の例外を投げることを期待している。
 
 3. I/Oってなんだよ
+
+
+4. build_runner実行前にproviderを定義していくときに、補完効かなくてだるい
+  → flutter pub run build_runner watch --delete-conflicting-outputs コマンドだと常時build_runnerが生成すべき差分を監視し、都度自動生成してくれるので、補完が効くようになって激アツ
+
+
+5. _$JokeViewModelってなに？ extedsってなに？継承？ implimentとの違いは？
+
+6. なんでリポジトリのProvider内も依存する local, cache, api provider をwatchしているの？ 一度読んだらそれで終わりでよく読んだらそれで終わりで良くなそれで終わりで良くないか？
+→ 他の画面で api, local cacheを別のdatasourceに切り替えた際に、それに察知してripositryProviderを提供してほしいから。
+↑ ぶっちゃけ現時点で、そんなことある？？って感じ
+↑テスト時とか！ 環境切り替える際とか！ プロダクトコードを普通に実行していく分には変わることはないけど、テスト時とかプロダクトコードを実行した上で、mockのdatasourceに切り替える必要があるので、そのときproviderが察知してくれる！
+
+*だから、基本的に依存しているProviderにはref.watchしとくのが無難！*
+
+
+ref.readはイベントハンドラーとかで一瞬使いたい（変化しない）ときに使う
